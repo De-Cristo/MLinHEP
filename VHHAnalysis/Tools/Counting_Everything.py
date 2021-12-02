991f21fc2d17853f9151d88bc948ffbb70ef596a
ZhangLicheng <zhanglic@node05.phy.pku.edu.cn>
2021-12-02 08:14:56 +0800

    1.debugging CountingTool

--- VHHAnalysis/Tools/Counting_Everything.py
+++ VHHAnalysis/Tools/Counting_Everything.py
@@ -6,18 +6,17 @@ from multiprocessing import Pool
 import subprocess
 import argparse
 
-R.EnableImplicitMT(8)
+R.EnableImplicitMT()
 
 def counter(_sample):
     for _rhh_name, _rhh in rHH_region_dict.items():
         for _zmass_name, _zmass in Zmass_region_dict.items():
             for _btag_name, _btag in btag_multiplicity_dict.items():
-                print('{0}_Zll_{1}_{2}_{3} : '.format(_sample, _rhh_name, _btag_name, _zmass_name) + ' Start Checking...')
-                rdf_dict['{0}_Zll_{1}_{2}_{3}'.format(_sample, _rhh_name, _btag_name, _zmass_name)] = rdf_dict[_sample].Filter(Zll).Filter(_rhh).Filter(_btag).Filter(_zmass)
-                print('Raw events number: {0}_Zll_{1}_{2}_{3} : '.format(_sample, _rhh_name, _btag_name, _zmass_name) + str(rdf_dict['{0}_Zll_{1}_{2}_{3}'.format(_sample, _rhh_name, _btag_name, _zmass_name)].Count().GetValue())+'\n')
-                print('Events number: {0}_Zll_{1}_{2}_{3} : '.format(_sample, _rhh_name, _btag_name, _zmass_name) + str(rdf_dict['{0}_Zll_{1}_{2}_{3}'.format(_sample, _rhh_name, _btag_name, _zmass_name)].Filter('weight<4').Sum('weight').GetValue())+'\n')
-                print('Events number(Keep large weight events): {0}_Zll_{1}_{2}_{3} : '.format(_sample, _rhh_name, _btag_name, _zmass_name) + str(rdf_dict['{0}_Zll_{1}_{2}_{3}'.format(_sample, _rhh_name, _btag_name, _zmass_name)].Filter('weight<4').Sum('weight').GetValue())+'\n')
-                print('{0}_Zll_{1}_{2}_{3} : '.format(_sample, _rhh_name, _btag_name, _zmass_name) + ' Done :)')
+                print('{0}_{1}_{2}_{3}_{4} : '.format(_sample, args.lcha, _rhh_name, _btag_name, _zmass_name) + ' Start Checking... \n')
+                rdf_dict['{0}_{1}_{2}_{3}_{4}'.format(_sample, args.lcha, _rhh_name, _btag_name, _zmass_name)] = rdf_dict[_sample].Filter(lepton_channel).Filter(_rhh).Filter(_btag).Filter(_zmass)
+#                 print('Raw events number: {0}_{1}_{2}_{3}_{4} : '.format(_sample,args.lcha, _rhh_name, _btag_name, _zmass_name) + str(rdf_dict['{0}_{1}_{2}_{3}_{4}'.format(_sample,args.lcha, _rhh_name, _btag_name, _zmass_name)].Count().GetValue())+'\n')
+                print('Events number: {0}_{1}_{2}_{3}_{4} : '.format(_sample,args.lcha, _rhh_name, _btag_name, _zmass_name) + str(rdf_dict['{0}_{1}_{2}_{3}_{4}'.format(_sample,args.lcha, _rhh_name, _btag_name, _zmass_name)].Filter('weight > 0 && weight<4').Sum('weight').GetValue())+'\n')
+                print('{0}_{1}_{2}_{3}_{4} : '.format(_sample,args.lcha, _rhh_name, _btag_name, _zmass_name) + ' Done :) \n')
                 
 def subprocessWrapper(c):
     subprocess.call(c, shell=True)
@@ -30,14 +29,12 @@ args = parser.parse_args()
 path = args.ndir
 
 if args.lcha == 'Zll':
-    lepton_channel = "isZee || isZmm"
+    lepton_channel = "(isZee || isZmm)"
 elif args.lcha == 'Znn':
     lepton_channel = "isZnn"
 else:
     print('We only support for Zll and Znn channel temporarily')
     exit(0)
-
-os.system('mkdir -p ./temp')
 
 file_dict = {}
 rdf_dict = {}
@@ -47,17 +44,17 @@ legend_dict = {}
 
 rHH_region_dict = {
         'rHH_SR' : "VHH_rHH<25",
-        'rHH_CR' : "VHH_rHH>25 && VHH_rHH<50",
-        'rHH_SB' : "VHH_rHH>50",
+#         'rHH_CR' : "VHH_rHH>25 && VHH_rHH<50",
+#         'rHH_SB' : "VHH_rHH>50",
 }
 
 Zmass_region_dict = {
-        'TT_CR'  : "V_mass<75 || V_mass>105",
+#         'TT_CR'  : "V_mass<75 || V_mass>105",
         'Z_mass'  : "V_mass>75 && V_mass<105",
 }
 
 btag_multiplicity_dict = {
-        'two_btag' : "VHH_nBJets == 2",
+#         'two_btag' : "VHH_nBJets == 2",
         'three_btag' : "VHH_nBJets == 3",
         'four_btag' : "VHH_nBJets == 4",
 }
@@ -69,7 +66,7 @@ file_list_for_TT = ['TT_AllHadronic','TT_DiLep_NoPSWeights','TT_SingleLep']
 file_list_for_ttbb = ['ttbb_AllHadronic','ttbb_DiLep','ttbb_SingleLep']
 file_list_DY = ['DYToLL_M10-50_Inc_madgraph','DYToLL_M-50_Inc_madgraph','DYToLL_M-50_HT70to100_madgraph','DYToLL_M-50_HT100to200_madgraph','DYToLL_M-50_HT200to400_madgraph','DYToLL_M-50_HT400to600_madgraph',\
                 'DYToLL_M-50_HT600to800_madgraph','DYToLL_M-50_HT800to1200_madgraph','DYToLL_M-50_HT1200to2500_madgraph','DYToLL_M-50_HT2500toInf_madgraph']
-file_list_ZHH = ['ZHHTo4B_CV_0_5_C2V_1_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_0_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_0_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_2_0','ZHHTo4B_CV_1_0_C2V_2_0_C3_1_0','ZHHTo4B_CV_1_5_C2V_1_0_C3_1_0']
+file_list_ZHH = ['ZHHTo4B_CV_0_5_C2V_1_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_0_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_0_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_2_0','ZHHTo4B_CV_1_0_C2V_2_0_C3_1_0','ZHHTo4B_CV_1_5_C2V_1_0_C3_1_0','ZHHTo4B_CV_1_0_C2V_1_0_C3_20_0']
 file_list_Other = ['ST_s-c_4f_lep','ST_t-channel_eleDecays','ST_t-channel_muDecays','ST_t-channel_tauDecays','ST_tW_antitop_5f_noFullHad','ST_tW_top_5f_noFullHad',\
                    'ttHTobb_M125','TTWLNuJets','TTWQQJets','TTZLLNuNuJets','TTZQQJets']
 file_list_Data = ['Run2018_DoubleMu_ReMiniAOD','Run2018_EG_ReMiniAOD']
@@ -82,28 +79,28 @@ string_list_for_ZHH = []
 string_list_for_Data = []
 
 for _file in file_list_for_TT:
-    string_list_for_TT.append('./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_TT.append('{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_TT)
 
 for _file in file_list_for_ttbb:
-    string_list_for_ttbb.append('./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_ttbb.append('{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_ttbb)
 
 for _file in file_list_DY:
-    string_list_for_DY.append('./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_DY.append('{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_DY)
 
 for _file in file_list_Other:
-    string_list_for_Other.append('./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_Other.append('{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_Other)
 
 for _file in file_list_Data:
-    string_list_for_Data.append('./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_Data.append('{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_Data)
 
 for _file in file_list_ZHH:
-    string_list_for_ZHH.append('./{0}/{1}/*.root'.format(path,_file))
-    rdf_dict[_file] = R.RDataFrame('Events','./{0}/{1}/*.root'.format(path,_file))
+    string_list_for_ZHH.append('{0}/{1}/*.root'.format(path,_file))
+    rdf_dict[_file] = R.RDataFrame('Events','{0}/{1}/*.root'.format(path,_file))
 print(string_list_for_ZHH)    
 
 rdf_dict['TT'] = R.RDataFrame('Events',string_list_for_TT)
@@ -116,7 +113,7 @@ rdf_dict['Data'] = R.RDataFrame('Events',string_list_for_Data)
 sample_list = ['TT', 'ttbb', 'DY', 'Other', 'Data']
 sample_list+=file_list_ZHH
 
-print(sample_list)
+sample_list = ['TT', 'ttbb', 'DY', 'ZHHTo4B_CV_1_0_C2V_1_0_C3_1_0']
 
 with Pool(12) as p:
     p.map(counter, sample_list)
