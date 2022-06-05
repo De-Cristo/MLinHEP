896ab222fad34aee552865ae701c0e3b64c84bc2
De-Cristo <johnsonzhang9711@163.com>
2022-06-05 17:17:01 +0800

    null

--- CoTools/XGB2TMVA/TMVA_train.py
+++ CoTools/XGB2TMVA/TMVA_train.py
@@ -7,9 +7,9 @@ R.EnableImplicitMT()
 R.TMVA.Tools.Instance()
 R.TMVA.PyMethodBase.PyInitialize()
 
-sig_file_name  = "./RwT_Training/Train_Sample/DY_SB_4b.root"
-bkg_file_name  = "./RwT_Training/Train_Sample/DY_SB_2b.root"
-outfileName    = "./TMVA_middle_RwT_DY_2TO4_model.root"
+sig_file_name  = "./Sample/zmerged.root"
+bkg_file_name  = "./Sample/zmerged.root"
+outfileName    = "./zmerged_middle_TMVA.root"
 
 input_sig_file = R.TFile(sig_file_name, "CACHEREAD")
 input_bkg_file = R.TFile(bkg_file_name, "CACHEREAD")
@@ -24,52 +24,17 @@ factory        = R.TMVA.Factory("TMVAClassification", outputFile,\
 
 dataloader     = R.TMVA.DataLoader("dataset")
 
-# train_var = [('dilep_dPhi','F'),('dilep_dEta','F'), ('ptl1OVERptl0','F'), ('ptl0OVERV_mass','F'),\
-#              ('VHH_Vreco4j_HT','F'), ('VHH_HH_dR','F'), ('VHH_V_HH_pT_Ratio','F'),('V_mass','F'),\
-#              ('VHH_H1_pT','F'), ('VHH_HH_pT','F'), ('V_pt','F'), \
-#              ('VHH_H1_m','F'),('VHH_HH_m','F'), \
-#              ('VHH_H1_e','F'),('VHH_HH_e','F'), \
-#              ('VHH_V_H1_dPhi','F'), ('VHH_V_HH_dPhi','F'), ('VHH_HH_deta','F'), \
-#              ('No3_btag_pt','F'), ('No4_btag_pt','F') \
-#             ]
-# train_var = [('V_mass','F'),('VHH_H1_m','F'), ('VHH_Vreco4j_HT','F'), ('VHH_HH_m','F'),\
-#              ('VHH_HH_pT','F'), ('No3_btag_pt','F'), ('VHH_H1_pT','F'),('VHH_V_HH_pT_Ratio','F')\
-#             ]
+# variable("deta_2mu2bj", ";#Delta #eta ;",     [25, 0., 5.])
+# variable("pt2bj_ov_m2bj", ";pT(bb)/m(bb);",     [20, 0., 6.])
+# variable("pt2bj_ov_mbbmm", ";pT(bb)/m(bbmm);",     [20, 0., 1])
+# variable("pt2mu_ov_m2mu",       ";pT(bb)/m(bb);",   [30, 0., 6.])
+# variable("scalar_hh_pt",       ";H_{T}_HH [GeV];",             [20, 0., 1200.])
 
-# dataloader.AddVariable("VHH_H2H1_pt_ratio", "VHH_H2H1_pt_ratio", "units", 'F' )
-# dataloader.AddVariable("VHH_HH_m", "VHH_HH_m", "units", 'F' )
-# dataloader.AddVariable("selLeptons_pt_0", "selLeptons_pt_0", "units", 'F' )
-# dataloader.AddVariable("dilep_dPhi", "dilep_dPhi", "units", 'F' )
-# dataloader.AddVariable("dilep_dEta", "dilep_dEta", "units", 'F' )
-# dataloader.AddVariable("ptl1OVERptl0", "ptl1OVERptl0", "units", 'F' )
-# dataloader.AddVariable("ptl0OVERV_mass", "ptl0OVERV_mass", "units", 'F' )
-# dataloader.AddVariable("VHH_V_H2_dPhi", "VHH_V_H2_dPhi", "units", 'F' )
-# dataloader.AddVariable("VHH_HH_dR", "VHH_HH_dR", "units", 'F' )
-# dataloader.AddVariable("VHH_H1_pT", "VHH_H1_pT", "units", 'F' )
-# dataloader.AddVariable("V_pt", "V_pt", "units", 'F' )
-# dataloader.AddVariable("VHH_H1_BJet_dR", "VHH_H1_BJet_dR", "units", 'F' )
-# dataloader.AddVariable("VHH_H2_BJet_dR", "VHH_H2_BJet_dR", "units", 'F' )
-
-dataloader.AddVariable("dilep_dPhi", "dilep_dPhi", "units", 'F' )
-dataloader.AddVariable("dilep_dEta", "dilep_dEta", "units", 'F' )
-dataloader.AddVariable("ptl1OVERptl0", "ptl1OVERptl0", "units", 'F' )
-dataloader.AddVariable("ptl0OVERV_mass", "ptl0OVERV_mass", "units", 'F' )
-dataloader.AddVariable("VHH_Vreco4j_HT", "VHH_Vreco4j_HT", "units", 'F' )
-dataloader.AddVariable("VHH_HH_dR", "VHH_HH_dR", "units", 'F' )
-dataloader.AddVariable("VHH_V_HH_pT_Ratio", "VHH_V_HH_pT_Ratio", "units", 'F' )
-dataloader.AddVariable("V_mass", "V_mass", "units", 'F' )
-dataloader.AddVariable("VHH_H1_pT", "VHH_H1_pT", "units", 'F' )
-dataloader.AddVariable("VHH_HH_pT", "VHH_HH_pT", "units", 'F' )
-dataloader.AddVariable("V_pt", "V_pt", "units", 'F' )
-dataloader.AddVariable("VHH_H1_m", "VHH_H1_m", "units", 'F' )
-dataloader.AddVariable("VHH_HH_m", "VHH_HH_m", "units", 'F' )
-dataloader.AddVariable("VHH_H1_e", "VHH_H1_e", "units", 'F' )
-dataloader.AddVariable("VHH_HH_e", "VHH_HH_e", "units", 'F' )
-dataloader.AddVariable("VHH_V_H1_dPhi", "VHH_V_H1_dPhi", "units", 'F' )
-dataloader.AddVariable("VHH_V_HH_dPhi", "VHH_V_HH_dPhi", "units", 'F' )
-dataloader.AddVariable("VHH_HH_deta", "VHH_HH_deta", "units", 'F' )
-dataloader.AddVariable("No3_btag_pt", "No3_btag_pt", "units", 'F' )
-dataloader.AddVariable("No4_btag_pt", "No4_btag_pt", "units", 'F' )
+dataloader.AddVariable("deta_2mu2bj", "deta_2mu2bj", "units", 'F' )
+dataloader.AddVariable("pt2bj_ov_m2bj", "pt2bj_ov_m2bj", "units", 'F' )
+dataloader.AddVariable("pt2bj_ov_mbbmm", "pt2bj_ov_mbbmm", "units", 'F' )
+dataloader.AddVariable("pt2mu_ov_m2mu", "pt2mu_ov_m2mu", "units", 'F' )
+dataloader.AddVariable("scalar_hh_pt", "scalar_hh_pt", "units", 'F' )
 
 signalWeight     = 1.0
 backgroundWeight = 1.0
@@ -80,13 +45,10 @@ dataloader.AddBackgroundTree( input_bkg_tree,     backgroundWeight )
 dataloader.SetSignalWeightExpression    ("weight")
 dataloader.SetBackgroundWeightExpression("weight")
 
-cuta = R.TCut()
-cutb = R.TCut()
+cuts = R.TCut("dsid==002")
+cutb = R.TCut("dsid>215&&dsid<225")
 
-cuta = " "
-cutb = " "
-
-dataloader.PrepareTrainingAndTestTree(R.TCut("weight<10"), "nTrain_Signal=3500:nTrain_Background=300000:nTest_Signal=500:nTest_Background=40000:SplitMode=Random:NormMode=None:!V")
+dataloader.PrepareTrainingAndTestTree(cuts, cutb, "nTrain_Signal=90000:nTrain_Background=500000:nTest_Signal=5650:nTest_Background=39005:SplitMode=Random:NormMode=None:!V")
 
 factory.BookMethod(dataloader, R.TMVA.Types.kBDT, "BDTG", "!H:!V:NTrees=200:MinNodeSize=5%:BoostType=Grad:Shrinkage=0.1:UseBaggedBoost:BaggedSampleFraction=0.7:nCuts=10:MaxDepth=3")
 
--- VHHAnalysis/Tools/HiggsCombine/ReadCardEx.sh
+++ VHHAnalysis/Tools/HiggsCombine/ReadCardEx.sh
@@ -5,6 +5,44 @@ export VHH_MET_CARD="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyW
 export VHH_FH_CARD="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/vhh_run2_combine/datacards_run2/bbbb_vhh/FH/*.txt | sed 's/ /,/g')"
 
 find . -name "card*.txt" -type f -exec sh -c "echo '* autoMCStats 0' >> {}" \;
+CMS_TTB_norm2016               rateParam  *          TTB        1
+CMS_TT_norm2016                rateParam  *          TT         1
+CMS_DY_norm2016                rateParam  *          DY         1
+
+CMS_DY_norm_2018               rateParam  *          DY         1
+CMS_TT_norm_2018               rateParam  *          TT         1
+CMS_TT_norm_2018               rateParam  *          TTB        1
+
+combineCards.py card_VhadHH_VHH_2016_kl=FH/card_VhadHH_VHH_2016_kl.txt card_VhadHH_VHH_2017_kl=FH/card_VhadHH_VHH_2017_kl.txt card_VhadHH_VHH_2018_kl=FH/card_VhadHH_VHH_2018_kl.txt card_VhadHH_VHH_2016_kVV=FH/card_VhadHH_VHH_2016_kVV.txt card_VhadHH_VHH_2017_kVV=FH/card_VhadHH_VHH_2017_kVV.txt card_VhadHH_VHH_2018_kVV=FH/card_VhadHH_VHH_2018_kVV.txt vhh4b_Wen_5_13TeV2016=SL/vhh4b_Wen_5_13TeV2016.txt vhh4b_Wen_5_13TeV2017=SL/vhh4b_Wen_5_13TeV2017.txt vhh4b_Wen_5_13TeV2018=SL/vhh4b_Wen_5_13TeV2018.txt vhh4b_Wen_6_13TeV2016=SL/vhh4b_Wen_6_13TeV2016.txt vhh4b_Wen_6_13TeV2017=SL/vhh4b_Wen_6_13TeV2017.txt vhh4b_Wen_6_13TeV2018=SL/vhh4b_Wen_6_13TeV2018.txt vhh4b_Wen_7_13TeV2016=SL/vhh4b_Wen_7_13TeV2016.txt vhh4b_Wen_7_13TeV2017=SL/vhh4b_Wen_7_13TeV2017.txt vhh4b_Wen_7_13TeV2018=SL/vhh4b_Wen_7_13TeV2018.txt vhh4b_Wen_8_13TeV2016=SL/vhh4b_Wen_8_13TeV2016.txt vhh4b_Wen_8_13TeV2017=SL/vhh4b_Wen_8_13TeV2017.txt vhh4b_Wen_8_13TeV2018=SL/vhh4b_Wen_8_13TeV2018.txt vhh4b_Wmn_9_13TeV2016=SL/vhh4b_Wmn_9_13TeV2016.txt vhh4b_Wmn_9_13TeV2017=SL/vhh4b_Wmn_9_13TeV2017.txt vhh4b_Wmn_9_13TeV2018=SL/vhh4b_Wmn_9_13TeV2018.txt vhh4b_Wmn_10_13TeV2016=SL/vhh4b_Wmn_10_13TeV2016.txt vhh4b_Wmn_10_13TeV2017=SL/vhh4b_Wmn_10_13TeV2017.txt vhh4b_Wmn_10_13TeV2018=SL/vhh4b_Wmn_10_13TeV2018.txt vhh4b_Wmn_11_13TeV2016=SL/vhh4b_Wmn_11_13TeV2016.txt vhh4b_Wmn_11_13TeV2017=SL/vhh4b_Wmn_11_13TeV2017.txt vhh4b_Wmn_11_13TeV2018=SL/vhh4b_Wmn_11_13TeV2018.txt vhh4b_Wmn_12_13TeV2016=SL/vhh4b_Wmn_12_13TeV2016.txt vhh4b_Wmn_12_13TeV2017=SL/vhh4b_Wmn_12_13TeV2017.txt vhh4b_Wmn_12_13TeV2018=SL/vhh4b_Wmn_12_13TeV2018.txt vhh4b_Znn_1_13TeV2016=MET/vhh4b_Znn_1_13TeV2016.txt vhh4b_Znn_1_13TeV2017=MET/vhh4b_Znn_1_13TeV2017.txt vhh4b_Znn_1_13TeV2018=MET/vhh4b_Znn_1_13TeV2018.txt vhh4b_Znn_2_13TeV2016=MET/vhh4b_Znn_2_13TeV2016.txt vhh4b_Znn_2_13TeV2017=MET/vhh4b_Znn_2_13TeV2017.txt vhh4b_Znn_2_13TeV2018=MET/vhh4b_Znn_2_13TeV2018.txt vhh4b_Znn_3_13TeV2016=MET/vhh4b_Znn_3_13TeV2016.txt vhh4b_Znn_3_13TeV2017=MET/vhh4b_Znn_3_13TeV2017.txt vhh4b_Znn_3_13TeV2018=MET/vhh4b_Znn_3_13TeV2018.txt vhh4b_Znn_4_13TeV2016=MET/vhh4b_Znn_4_13TeV2016.txt vhh4b_Znn_4_13TeV2017=MET/vhh4b_Znn_4_13TeV2017.txt vhh4b_Znn_4_13TeV2018=MET/vhh4b_Znn_4_13TeV2018.txt card_hists_2018_R_Zll_Klbdt_High_3b_bin_SR_Z=DL/card_hists_2018_R_Zll_Klbdt_High_3b_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_4b_bin_SR_Z=DL/card_hists_2018_R_Zll_Klbdt_High_4b_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_3b_bin_SR_Z=DL/card_hists_2018_R_Zll_Klbdt_SM_3b_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_4b_bin_SR_Z=DL/card_hists_2018_R_Zll_Klbdt_SM_4b_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_3b_bin_SR_Z=DL/card_hists_2017_R_Zll_Klbdt_High_3b_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_4b_bin_SR_Z=DL/card_hists_2017_R_Zll_Klbdt_High_4b_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_3b_bin_SR_Z=DL/card_hists_2017_R_Zll_Klbdt_SM_3b_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_4b_bin_SR_Z=DL/card_hists_2017_R_Zll_Klbdt_SM_4b_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_3b_bin_SR_Z=DL/card_hists_2016_R_Zll_Klbdt_High_3b_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_4b_bin_SR_Z=DL/card_hists_2016_R_Zll_Klbdt_High_4b_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_3b_bin_SR_Z=DL/card_hists_2016_R_Zll_Klbdt_SM_3b_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_4b_bin_SR_Z=DL/card_hists_2016_R_Zll_Klbdt_SM_4b_bin_SR_Z.txt > vhh4b_All.txt
+
+combineCards.py card_VhadHH_VHH_2016_kl=FH/card_VhadHH_VHH_2016_kl.txt card_VhadHH_VHH_2017_kl=FH/card_VhadHH_VHH_2017_kl.txt card_VhadHH_VHH_2018_kl=FH/card_VhadHH_VHH_2018_kl.txt card_VhadHH_VHH_2016_kVV=FH/card_VhadHH_VHH_2016_kVV.txt card_VhadHH_VHH_2017_kVV=DL/card_VhadHH_VHH_2017_kVV.txt card_VhadHH_VHH_2018_kVV=FH/card_VhadHH_VHH_2018_kVV.txt > vhh4b_All.txt
+
+combineCards.py card_hists_2018_R_Zll_Klbdt_High_3b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_3b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_4b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_4b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_3b_ee_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_High_3b_ee_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_4b_ee_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_High_4b_ee_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_3b_ee_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_High_3b_ee_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_4b_ee_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_High_4b_ee_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_3b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_3b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_4b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_4b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_3b_mm_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_High_3b_mm_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_High_4b_mm_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_High_4b_mm_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z.txt card_hists_2017_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z=card_hists_2017_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_3b_mm_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_High_3b_mm_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_High_4b_mm_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_High_4b_mm_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z.txt card_hists_2016_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z=card_hists_2016_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z.txt > vhh4b_Zll.txt
+
+combineCards.py card_hists_2018_R_Zll_Klbdt_High_3b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_3b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_4b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_4b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_3b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_4b_ee_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_3b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_3b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_High_4b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_High_4b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_3b_mm_bin_SR_Z.txt card_hists_2018_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z=card_hists_2018_R_Zll_Klbdt_SM_4b_mm_bin_SR_Z.txt > vhh4b_Zll.txt
+
+text2workspace.py vhh4b_Zll.txt
+combineTool.py -M Impacts -d vhh4b_Zll.root -t -1 --expectSignal 0 --doInitialFit --allPars -m 1 -n t0 --cminDefaultMinimizerStrategy 0 --rMin -40 --rMax 40
+combineTool.py -M Impacts -d vhh4b_Zll.root -o impacts_t0.json -t -1 --expectSignal 0 --doFits -m 1 -n t0 --parallel 20 --task-name t0 --cminDefaultMinimizerStrategy 0 --rMin -40 --rMax 40 >& /dev/null
+combineTool.py -M Impacts -d vhh4b_Zll.root -m 1 -n t0 -o impacts_t0.json
+plotImpacts.py -i  impacts_t0.json -o  impacts_t0
+rm *paramFit*
+
+combine -M FitDiagnostics vhh4b_Zll.txt --saveShapes -t -1 --expectSignal=1 --saveWithUncertainties -n test --plots
+combineTool.py -M FitDiagnostics vhh4b_Zll.txt --saveShapes -t -1 --expectSignal 0 --saveWithUncertainties -n test --parallel 20 --plots
+combine -M FitDiagnostics vhh4b_Zll.txt --saveShapes -t -1 --expectSignal=1 --saveWithUncertainties -n test --parallel 20 --plots
+
+
+combineCards.py vhh4b_Wen_5_13TeV2016=SL/vhh4b_Wen_5_13TeV2016.txt vhh4b_Wen_5_13TeV2017=SL/vhh4b_Wen_5_13TeV2017.txt vhh4b_Wen_5_13TeV2018=SL/vhh4b_Wen_5_13TeV2018.txt vhh4b_Wen_6_13TeV2016=SL/vhh4b_Wen_6_13TeV2016.txt vhh4b_Wen_6_13TeV2017=SL/vhh4b_Wen_6_13TeV2017.txt vhh4b_Wen_6_13TeV2018=SL/vhh4b_Wen_6_13TeV2018.txt vhh4b_Wen_7_13TeV2016=SL/vhh4b_Wen_7_13TeV2016.txt vhh4b_Wen_7_13TeV2017=SL/vhh4b_Wen_7_13TeV2017.txt vhh4b_Wen_7_13TeV2018=SL/vhh4b_Wen_7_13TeV2018.txt vhh4b_Wen_8_13TeV2016=SL/vhh4b_Wen_8_13TeV2016.txt vhh4b_Wen_8_13TeV2017=SL/vhh4b_Wen_8_13TeV2017.txt vhh4b_Wen_8_13TeV2018=SL/vhh4b_Wen_8_13TeV2018.txt vhh4b_Wmn_9_13TeV2016=SL/vhh4b_Wen_9_13TeV2016.txt vhh4b_Wmn_9_13TeV2017=SL/vhh4b_Wmn_9_13TeV2017.txt vhh4b_Wmn_9_13TeV2018=SL/vhh4b_Wmn_9_13TeV2018.txt vhh4b_Wmn_10_13TeV2016=SL/vhh4b_Wmn_10_13TeV2016.txt vhh4b_Wmn_10_13TeV2017=SL/vhh4b_Wmn_10_13TeV2017.txt vhh4b_Wmn_10_13TeV2018=SL/vhh4b_Wmn_10_13TeV2018.txt vhh4b_Wmn_11_13TeV2016=SL/vhh4b_Wmn_11_13TeV2016.txt vhh4b_Wmn_11_13TeV2017=SL/vhh4b_Wmn_11_13TeV2017.txt vhh4b_Wmn_11_13TeV2018=SL/vhh4b_Wmn_11_13TeV2018.txt vhh4b_Wmn_12_13TeV2016=SL/vhh4b_Wmn_12_13TeV2016.txt vhh4b_Wmn_12_13TeV2017=SL/vhh4b_Wmn_12_13TeV2017.txt vhh4b_Wmn_12_13TeV2018=SL/vhh4b_Wmn_12_13TeV2018.txt > vhh4b_All.txt
+
+combineCards.py  vhh4b_Znn_1_13TeV2016=MET/vhh4b_Znn_1_13TeV2016.txt vhh4b_Znn_1_13TeV2017=MET/vhh4b_Znn_1_13TeV2017.txt vhh4b_Znn_1_13TeV2018=MET/vhh4b_Znn_1_13TeV2018.txt vhh4b_Znn_2_13TeV2016=MET/vhh4b_Znn_2_13TeV2016.txt vhh4b_Znn_2_13TeV2017=MET/vhh4b_Znn_2_13TeV2017.txt vhh4b_Znn_2_13TeV2018=MET/vhh4b_Znn_2_13TeV2018.txt vhh4b_Znn_3_13TeV2016=MET/vhh4b_Znn_3_13TeV2016.txt vhh4b_Znn_3_13TeV2017=MET/vhh4b_Znn_3_13TeV2017.txt vhh4b_Znn_3_13TeV2018=MET/vhh4b_Znn_3_13TeV2018.txt vhh4b_Znn_4_13TeV2016=MET/vhh4b_Znn_4_13TeV2016.txt vhh4b_Znn_4_13TeV2017=MET/vhh4b_Znn_4_13TeV2017.txt vhh4b_Znn_4_13TeV2018=MET/vhh4b_Znn_4_13TeV2018.txt > vhh4b_All.txt
+
+text2workspace.py vhh4b_All.txt
+combineTool.py -M Impacts -d vhh4b_All.root -t -1 --expectSignal 0 --doInitialFit --allPars -m 1 -n t0 --cminDefaultMinimizerStrategy 0 --rMin -40 --rMax 40
+combineTool.py -M Impacts -d vhh4b_All.root -o impacts_t0.json -t -1 --expectSignal 0 --doFits -m 1 -n t0 --parallel 20 --task-name t0 --cminDefaultMinimizerStrategy 0 --rMin -40 --rMax 40 >& /dev/null
+combineTool.py -M Impacts -d vhh4b_All.root -m 1 -n t0 -o impacts_t0.json
+plotImpacts.py -i  impacts_t0.json -o  impacts_t0
+rm *paramFit*
 
 export VHH_DL_Run2_CARD="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/DL_0407/*/*.txt | sed 's/ /,/g')"
 export VHH_DL_16_CARD="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/DL_0407/*2016*/*.txt | sed 's/ /,/g')"
@@ -64,3 +102,46 @@ export DL_17_rwt="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWork
 export DL_16_rwt="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/DL_2016_rwt/*.txt | sed 's/ /,/g')"
 export DL_RunII_rwt="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/DL_201*_rwt/*.txt | sed 's/ /,/g')"
 export DL_18ScaleRunII_rwt="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/DL_2018_rwt_0421/*.txt | sed 's/ /,/g')"
+
+export DL_18_rwt_full_Syst="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/DL_2018_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_17_rwt_full_Syst="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/DL_2017_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_16_rwt_full_Syst="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/DL_2016_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_RunII_rwt_full_Syst="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/DL_RunII_full_syst/*.txt | sed 's/ /,/g')"
+
+export DL="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/VHH/DL/*.txt | sed 's/ /,/g')"
+export SL="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/VHH/SL/*.txt | sed 's/ /,/g')"
+export MET="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/VHH/MET/*.txt | sed 's/ /,/g')"
+export FH="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/VHH/FH/*.txt | sed 's/ /,/g')"
+export VHH="$(echo /afs/cern.ch/work/l/lichengz/private/VHH/upperlimit/CMSSW_10_2_13/src/MyWorkArea/VHH/*/*.txt | sed 's/ /,/g')"
+
+export DL_18_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_17_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2017_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_16_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2016_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_RunII_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_RunII_full_syst/*.txt | sed 's/ /,/g')"
+
+export DL="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/VHH/DL/*.txt | sed 's/ /,/g')"
+export SL="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/VHH/SL/*.txt | sed 's/ /,/g')"
+export MET="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/VHH/MET/*.txt | sed 's/ /,/g')"
+export FH="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/VHH/FH/*.txt | sed 's/ /,/g')"
+export VHH="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/VHH/*/*.txt | sed 's/ /,/g')"
+
+export DL_18_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_hbb="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_hbb/*.txt | sed 's/ /,/g')"
+
+export DL_18_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_9bins="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_9bins/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_eemm_9bins="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm_9bins/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_11bins="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_11bins/*.txt | sed 's/ /,/g')"
+
+
+export DL_18_rwt_full_Syst="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_eemm="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*.txt | sed 's/ /,/g')"
+
+
+export DL_18_rwt_full_Syst_eemm="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_ee="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*ee*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_mm="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*mm*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_3b="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*3b*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_4b="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*4b*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_SM="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*SM*.txt | sed 's/ /,/g')"
+export DL_18_rwt_full_Syst_High="$(echo /data/pubfs/zhanglic/workspace/CMSSW_10_2_13/src/MyWorkArea/new_v/DL_2018_rwt_full_syst_eemm/*High*.txt | sed 's/ /,/g')"
